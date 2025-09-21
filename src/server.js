61bacf44bbc807b67dbf5ff166ba27065ee29b88
tintin.raholm <tintin.raholm@arcada.fi>
2025-09-21 18:21:53 +0300

    server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
+const express = require('express')
+require('dotenv').config()
+const authRouter = require('./routes/auth')
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
 console.log("Hello Node")
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ msg: "Hej hej" })
+})
+
+app.use('/auth', authRouter)
+
+app.listen(PORT, () => {
+    try {
+        console.log(`Running on http://localhost:${PORT}`)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+    
+})
