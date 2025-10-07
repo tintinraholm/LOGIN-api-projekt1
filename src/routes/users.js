dd04b8e946d499800d78f34bc1834ca4113322af
tintin.raholm <tintin.raholm@arcada.fi>
2025-10-07 20:34:32 +0300

    prisma fix

--- src/routes/users.js
+++ src/routes/users.js
@@ -111,7 +111,7 @@ router.delete('/logout', async (req, res) => {
         console.log("refreshToken i localStorage:", localStorage.getItem("refreshToken"));
 
         if (refreshToken) {
-            await prisma.refresh_tokens.deleteMany({
+            await prisma.refresh_tokens.delete({
                 where: {
                     token: refreshToken
                 }
